09c59970c691f59a854b330f06dedbc98f27bee4
vibhor <vibhorwebsolutions@gmail.com>
2025-02-17 14:20:39 +0530

    Finished my first boot.dev project bookbot!

---
+++ main.py
@@ -0,0 +1,34 @@
+def main():  
+  book_path = "books/frankenstein.txt"
+  text = get_book_text(book_path)
+  num_words = get_num_words(text)
+  character_set = count_characters(text)  
+  print_report(book_path, num_words, character_set)
+
+def print_report(path, count, character_set):
+  char_to_check = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+  print(f"--- Begin report of {path} ---")
+  print(f"{count} words found in the document\n")
+  for char in character_set:
+    if char in char_to_check:      
+      print(f"The '{char}' character was found {character_set[char]} times")  
+  print("--- End report ---")
+
+def count_characters(text):
+  character_set = {}
+  text = text.lower()  
+  for c in text:    
+    if c not in character_set:
+      character_set[c] = 1
+    else:
+      character_set[c] += 1
+  return character_set
+
+def get_num_words(text):
+  return len(text.split())
+
+def get_book_text(path):
+  with open(path) as f:
+    return f.read()
+
+main()
